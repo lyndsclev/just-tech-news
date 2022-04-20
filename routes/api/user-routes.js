78e05385c702dbd8bdb92adf371419cb73de8849
lyndsclev <lyndsclev@gmail.com>
2022-04-20 16:59:14 -0400

    added password hash

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -52,6 +52,9 @@ router.put('/:id', (req, res) => {
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    
+    individualHooks: true, 
+
     where: {
       id: req.params.id
     }
